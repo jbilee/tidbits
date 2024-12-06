3357a9816c45e2f74727bdabfa90263e32090db5
jbilee <julishahn@gmail.com>
2024-12-06 17:16:40 +0900

    feat: Settings button

---
+++ src/components/hud/SettingsButton.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import cogIcon from "../../assets/cog.svg";
+</script>
+
+<button class="icon-button" type="button">
+  <img src={cogIcon} width={36} height={36} alt="Settings menu icon" />
+  Settings
+</button>
+
+<style>
+  .icon-button {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 0.25rem;
+    font-size: 0.75rem;
+  }
+</style>
