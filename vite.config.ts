d753035e8a250497ce34ca85439de2dcd412f982
jbilee <julishahn@gmail.com>
2024-12-08 13:49:37 +0900

    chore: Set up import alias settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
+import { resolve } from "path";
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  resolve: {
+    alias: [{ find: "@", replacement: resolve(__dirname, "./src") }],
+  },
 });
